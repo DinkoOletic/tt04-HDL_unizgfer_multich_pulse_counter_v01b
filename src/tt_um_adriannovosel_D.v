b0eb0fb8acaac4d03192b3af5ce8839b666294e2
adriannovosel <134148571+adriannovosel@users.noreply.github.com>
2023-07-06 13:14:51 +0200

    Update tt_um_adriannovosel_D.v

--- src/tt_um_adriannovosel_D.v
+++ src/tt_um_adriannovosel_D.v
@@ -1,6 +1,6 @@
 //light up the leds according to a counter to cycle through every one
 
-module tt_um_adriannovosel_D(input wire a, b, clk, ena, rst_n, output reg y);
+module tt_um_adriannovosel_D(input wire a, b, clk, ena, rst_n, ui_in, output reg y);
 
    always@(posedge clk) begin
       y <= a && b;
