7aeb930aeea142a6ab7075505bd7aaf5418ca20e
adriannovosel <134148571+adriannovosel@users.noreply.github.com>
2023-07-06 13:18:40 +0200

    Update tt_um_adriannovosel_D.v

--- src/tt_um_adriannovosel_D.v
+++ src/tt_um_adriannovosel_D.v
@@ -1,9 +1,16 @@
 //light up the leds according to a counter to cycle through every one
 
-module tt_um_adriannovosel_D(input wire a, b, clk, ena, rst_n, ui_in, output reg y);
+module tt_um_adriannovosel_D(input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // will go high when the design is enabled
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset);
 
    always@(posedge clk) begin
-      y <= a && b;
+      uo_out[0] <= uio_in[0] && uio_in[1];
    end
    
 endmodule // top
