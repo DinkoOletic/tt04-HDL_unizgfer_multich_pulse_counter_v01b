d8c95d2f3e2d016174e49dead9dd3efd0d9118c9
adriannovosel <134148571+adriannovosel@users.noreply.github.com>
2023-07-06 12:46:49 +0200

    Update and rename adriannovosel_D.v to tt_um_adriannovosel_D.v

--- src/tt_um_adriannovosel_D.v
+++ src/tt_um_adriannovosel_D.v
@@ -1,6 +1,6 @@
 //light up the leds according to a counter to cycle through every one
 
-module top(input a, b, output reg y);
+module tt_um_addriannovosel_D(input a, b, output reg y);
 
 assign y = a && b;
    
