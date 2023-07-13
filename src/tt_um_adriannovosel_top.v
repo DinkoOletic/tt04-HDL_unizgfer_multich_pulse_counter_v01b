5f0fa3a334c505bcafb0793827fd4d05a8ae08d0
adriannovosel <134148571+adriannovosel@users.noreply.github.com>
2023-07-13 17:50:16 +0200

    Update and rename tt_um_adriannovosel_D.v to tt_um_adriannovosel_top.v

--- src/tt_um_adriannovosel_top.v
+++ src/tt_um_adriannovosel_top.v
@@ -1,6 +1,6 @@
 //light up the leds according to a counter to cycle through every one
 
-module tt_um_adriannovosel_D(input  wire [7:0] ui_in,    // Dedicated inputs
+module tt_um_adriannovosel_top(input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
@@ -10,8 +10,8 @@ module tt_um_adriannovosel_D(input  wire [7:0] ui_in,    // Dedicated inputs
     input  wire       rst_n
     );// reset_n - low to reset);
 
-   always@(posedge clk) begin
-      uo_out[0] <= uio_in[0] && uio_in[1];
-   end
+    top top1(.reset(ui_in[0]), .clk(ui_in[1]), .ch1(ui_in[2]), .ch2(ui_in[3]), .ch3(ui_in[4]), .ch4(ui_in[5]), .RTC(ui_in[6]), 
+             .serial_out(uo_out[0]), .ovf_global(uo_out[1]), .ovf_RTC_out(uo_out[2]), .a0_out(uo_out[3]), .a1_out(uo_out[4]), 
+             .a2_out(uo_out[5]), .SL_out(uo_out[6]), .ovf_ch_out(uo_out[7]));
    
 endmodule // top
